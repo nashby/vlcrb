45557c5362d49138b38d4cbfdf6c98089516ba10
nashby <younash@gmail.com>
2011-05-01 19:40:56 +0300

    add example

---
+++ examples/libvlc_tutorial.rb
@@ -0,0 +1,8 @@
+# example from http://wiki.videolan.org/LibVLC_Tutorial
+require 'vlcrb'
+
+inst = VLC::LibVLC.libvlc_new(0, nil)
+m = VLC::Media.libvlc_media_new_path(inst, "examples/1.flv")
+mp = VLC::Media::Player.libvlc_media_player_new_from_media(m)
+VLC::Media::Player.libvlc_media_player_play(mp)
+sleep(100)
